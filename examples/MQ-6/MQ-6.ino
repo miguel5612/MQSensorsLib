eeca1ac43683a8a0a1e04ae38c2edb5650be0493
miguel5612 <miguelangel5612@gmail.com>
2019-07-09 17:09:48 -0500

    Added MQ-6 full example in comments

--- examples/MQ-6/MQ-6.ino
+++ examples/MQ-6/MQ-6.ino
@@ -26,16 +26,27 @@ MQUnifiedsensor MQ6(pin, type);
 
 void setup() {
   //init the sensor
+  /*****************************  MQInicializar****************************************
+  Input:  pin, type 
+  Output:  
+  Remarks: This function create the sensor object.
+  ************************************************************************************/ 
   MQ6.inicializar(); 
 }
 
 void loop() {
-  //Read the sensor
-  int read =  MQ6.readSensor();
-  //Print measurements
-  Serial.print("MQ6: ");
-  Serial.print(read);
-  Serial.println(" PPM");
-  //delay 1s to next measure
-  delay(1000);
+  /*****************************  MQReadSensor  ****************************************
+  Input:   Gas - Serial print flag
+  Output:  Value in PPM
+  Remarks: This function use readPPM to read the value in PPM the gas in the air.
+  ************************************************************************************/ 
+  //Read the sensor and print in serial port
+  //Lecture will be saved in lecture variable
+  int lecture =  MQ6.readSensor("", true); // Return CH4 concentration
+  // Options, uncomment where you need
+  //int lecture =  MQ6.readSensor("H2", true); // Return CH4 concentration
+  //int lecture =  MQ6.readSensor("LPG", true); // Return LPG concentration
+  //int lecture =  MQ6.readSensor("CH4", true); // Return CO concentration
+  //int lecture =  MQ6.readSensor("CO", true); // Return Alcohol concentration
+  //int lecture =  MQ6.readSensor("Alcohol", true); // Return smoke concentration
 }
