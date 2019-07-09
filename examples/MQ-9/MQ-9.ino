2b44b1fd989d1424b8e9fad650db0fe20c359219
miguel5612 <miguelangel5612@gmail.com>
2019-07-09 17:14:47 -0500

    Added MQ-8, MQ-9 full example in comments

--- examples/MQ-9/MQ-9.ino
+++ examples/MQ-9/MQ-9.ino
@@ -26,16 +26,25 @@ MQUnifiedsensor MQ9(pin, type);
 
 void setup() {
   //init the sensor
+  /*****************************  MQInicializar****************************************
+  Input:  pin, type 
+  Output:  
+  Remarks: This function create the sensor object.
+  ************************************************************************************/ 
   MQ9.inicializar(); 
 }
 
 void loop() {
-  //Read the sensor
-  int read =  MQ9.readSensor();
-  //Print measurements
-  Serial.print("MQ9: ");
-  Serial.print(read);
-  Serial.println(" PPM");
-  //delay 1s to next measure
-  delay(1000);
+ /*****************************  MQReadSensor  ****************************************
+  Input:   Gas - Serial print flag
+  Output:  Value in PPM
+  Remarks: This function use readPPM to read the value in PPM the gas in the air.
+  ************************************************************************************/ 
+  //Read the sensor and print in serial port
+  //Lecture will be saved in lecture variable
+  int lecture =  MQ8.readSensor("", true); // Return LPG concentration
+  // Options, uncomment where you need
+  //int lecture =  MQ8.readSensor("LPG", true); // Return LPG concentration
+  //int lecture =  MQ8.readSensor("CH4", true); // Return CO concentration
+  //int lecture =  MQ8.readSensor("CO", true); // Return Alcohol concentration
 }
