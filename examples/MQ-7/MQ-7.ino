fad888dca416d0bb89f7a9b4f1f214af5017d3e2
miguel5612 <miguelangelcu@ufps.edu.co>
2020-03-26 13:04:04 -0500

    Updated MQ-7

--- examples/MQ-7/MQ-7.ino
+++ examples/MQ-7/MQ-7.ino
@@ -1,14 +1,16 @@
 /*
   MQUnifiedsensor Library - reading an MQ7
 
-
-  Demonstrates the use a MQ7
-   sensor.
+  Demonstrates the use a MQ7 sensor.
   Library originally added 01 may 2019
   by Miguel A Califa, Yersson Carrillo, Ghiordy Contreras, Mario Rodriguez
  
   Added example
   modified 23 May 2019
+  by Miguel Califa 
+
+  Updated library usage
+  modified 26 March 2020
   by Miguel Califa 
 
  This example code is in the public domain.
@@ -19,66 +21,57 @@
 #include <MQUnifiedsensor.h>
 
 //Definitions
+#define placa "Arduino UNO"
+#define Voltage_Resolution 5
 #define pin A0 //Analog input 0 of your arduino
-#define type 7 //MQ7
+#define type "MQ-7" //MQ7
+#define ADC_Bit_Resolution 10 // For arduino UNO/MEGA/NANO
 //#define calibration_button 13 //Pin to calibrate your sensor
 
-
 //Declare Sensor
-MQUnifiedsensor MQ7(pin, type);
-
-//Variables
-float H2, LPG, CH4, CO, Alcohol;
+MQUnifiedsensor MQ7(placa, Voltage_Resolution, ADC_Bit_Resolution, pin, type);
 
 void setup() {
+  //Init the serial port communication - to debug the library
   Serial.begin(9600); //Init serial port
-   //init the sensor
-  /*****************************  MQInicializar****************************************
-  Input:  pin, type 
-  Output:  
-  Remarks: This function create the sensor object.
-  ************************************************************************************/ 
-  MQ7.inicializar(); 
-  //pinMode(calibration_button, INPUT);
+
+  //Set math model to calculate the PPM concentration and the value of constants
+  MQ7.setRegressionMethod("Exponential"); //_PPM =  a*ratio^b
+  MQ7.setA(99.042); MQ7.setB(-1.518); // Configurate the ecuation values to get CO concentration
+
+  /*
+    Exponential regression:
+  GAS     | a      | b
+  H2      | 69.014  | -1.374
+  LPG     | 700000000 | -7.703
+  CH4     | 60000000000000 | -10.54
+  CO      | 99.042 | -1.518
+  Alcohol | 40000000000000000 | -12.35
+  */
+  
+  // Calibration setup
+  MQ7.setR0(4);
+
+  /* 
+    //If the RL value is different from 10K please assign your RL value with the following method:
+    MQ7.setRL(10);
+  */
+
+  /*****************************  MQ Init ********************************************/ 
+  //Remarks: Configure the pin of arduino as input.
+  /************************************************************************************/ 
+  MQ7.init(); 
+  /*****************************  MQ Init ********************************************/ 
+  //Input: setup flag, if this function are on setup will print the headers (Optional - Default value: False)
+  //Output: print on serial port the information about sensor and sensor readings
+  //Remarks: Configure the pin of arduino as input.
+  /************************************************************************************/ 
+  MQ7.serialDebug(true);
 }
 
 void loop() {
-  MQ7.update(); // Update data, the arduino will be read the voltaje in the analog pin
-  /* 
-    //Si el valor de RL es diferente a 10K por favor asigna tu valor de RL con el siguiente metodo:
-    MQ7.setRL(10);
-  */
-  /*
-  //Rutina de calibracion - Uncomment if you need (setup too and header)
-  if(calibration_button)
-  {
-    float R0 = MQ7.calibrate();
-    MQ7.setR0(R0);
-  }
-  */
-  /*****************************  MQReadSensor  ****************************************
-  Input:   Gas - Serial print flag
-  Output:  Value in PPM
-  Remarks: This function use readPPM to read the value in PPM the gas in the air.
-  ************************************************************************************/ 
-  //Read the sensor and print in serial port
-  //Lecture will be saved in lecture variable
-  //float lecture =  MQ7.readSensor("", true); // Return CH4 concentration
-  // Options, uncomment where you need
-  H2 =  MQ7.readSensor("H2"); // Return CH4 concentration
-  LPG =  MQ7.readSensor("LPG"); // Return LPG concentration
-  CH4 =  MQ7.readSensor("CH4"); // Return CH4 concentration
-  CO =  MQ7.readSensor("CO"); // Return CO concentration
-  Alcohol =  MQ7.readSensor("Alcohol"); // Return Alcohol concentration
-  
-  Serial.println("***************************");
-  Serial.println("Lectures for MQ-7");
-  Serial.print("Volt: ");Serial.print(MQ7.getVoltage(false));Serial.println(" V"); 
-  Serial.print("R0: ");Serial.print(MQ7.getR0());Serial.println(" Ohm"); 
-  Serial.print("H2: ");Serial.print(H2,2);Serial.println(" ppm");
-  Serial.print("LPG: ");Serial.print(LPG,2);Serial.println(" ppm");
-  Serial.print("CH4: ");Serial.print(CH4,2);Serial.println(" ppm");
-  Serial.print("CO: ");Serial.print(CO,2);Serial.println(" ppm");
-  Serial.print("Alcohol: ");Serial.print(Alcohol,2);Serial.println(" ppm");
-  Serial.println("***************************");  
+  MQ7.update(); // Update data, the arduino will be read the voltage on the analog pin
+  MQ7.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+  MQ7.serialDebug(); // Will print the table on the serial port
+  delay(500); //Sampling frequency
 }
