e9e1169cc75caae41502e65d5f6e38bd0f6d7166
miguel5612 <miguelangelcu@ufps.edu.co>
2020-03-26 12:51:02 -0500

    Fixed MQ-6

--- examples/MQ-6/MQ-6.ino
+++ examples/MQ-6/MQ-6.ino
@@ -1,5 +1,5 @@
 /*
-  MQUnifiedsensor Library - reading an MQ4
+  MQUnifiedsensor Library - reading an MQ6
 
   Demonstrates the use a MQ6 sensor.
   Library originally added 01 may 2019
@@ -7,6 +7,10 @@
  
   Added example
   modified 23 May 2019
+  by Miguel Califa 
+
+  Updated library usage
+  modified 26 March 2020
   by Miguel Califa 
 
  This example code is in the public domain.
@@ -17,65 +21,56 @@
 #include <MQUnifiedsensor.h>
 
 //Definitions
+#define placa "Arduino UNO"
+#define Voltage_Resolution 5
 #define pin A0 //Analog input 0 of your arduino
-#define type 6 //MQ6
+#define type "MQ-6" //MQ6
+#define ADC_Bit_Resolution 10 // For arduino UNO/MEGA/NANO
 //#define calibration_button 13 //Pin to calibrate your sensor
 
 //Declare Sensor
-MQUnifiedsensor MQ6(pin, type);
-
-//Variables
-float H2, LPG, CH4, CO, Alcohol;
+MQUnifiedsensor MQ6(placa, Voltage_Resolution, ADC_Bit_Resolution, pin, type);
 
 void setup() {
+  //Init the serial port communication - to debug the library
   Serial.begin(9600); //Init serial port
-  //init the sensor
-  /*****************************  MQInicializar****************************************
-  Input:  pin, type 
-  Output:  
-  Remarks: This function create the sensor object.
-  ************************************************************************************/ 
-  MQ6.inicializar(); 
-  //pinMode(calibration_button, INPUT);
+
+  //Set math model to calculate the PPM concentration and the value of constants
+  MQ6.setRegressionMethod("Exponential"); //_PPM =  a*ratio^b
+  MQ6.setA(2127.2); MQ6.setB(-2.526); // Configurate the ecuation values to get CH4 concentration
+  /*
+    Exponential regression:
+  GAS     | a      | b
+  H2      | 88158  | -3.597
+  LPG     | 1009.2 | -2.35
+  CH4     | 2127.2 | -2.526
+  CO      | 1000000000000000 | -13.5
+  Alcohol | 50000000 | -6.017
+  */
+  
+  // Calibration setup
+  MQ6.setR0(13.4285714);
+
+  /* 
+    //If the RL value is different from 10K please assign your RL value with the following method:
+    MQ6.setRL(10);
+  */
+
+  /*****************************  MQ Init ********************************************/ 
+  //Remarks: Configure the pin of arduino as input.
+  /************************************************************************************/ 
+  MQ6.init(); 
+  /*****************************  MQ Init ********************************************/ 
+  //Input: setup flag, if this function are on setup will print the headers (Optional - Default value: False)
+  //Output: print on serial port the information about sensor and sensor readings
+  //Remarks: Configure the pin of arduino as input.
+  /************************************************************************************/ 
+  MQ6.serialDebug(true);
 }
 
 void loop() {
-  MQ6.update(); // Update data, the arduino will be read the voltaje in the analog pin
-  /* 
-    //Si el valor de RL es diferente a 10K por favor asigna tu valor de RL con el siguiente metodo:
-    MQ6.setRL(10);
-  */
-  /*
-  //Rutina de calibracion - Uncomment if you need (setup too and header)
-  if(calibration_button)
-  {
-    float R0 = MQ6.calibrate();
-    MQ6.setR0(R0):
-  }
-  */
-  /*****************************  MQReadSensor  ****************************************
-  Input:   Gas - Serial print flag
-  Output:  Value in PPM
-  Remarks: This function use readPPM to read the value in PPM the gas in the air.
-  ************************************************************************************/ 
-  //Read the sensor and print in serial port
-  //Lecture will be saved in lecture variable
-  //float lecture =  MQ6.readSensor("", true); // Return CH4 concentration
-  // Options, uncomment where you need
-  H2 =  MQ6.readSensor("H2"); // Return H2 concentration
-  LPG =  MQ6.readSensor("LPG"); // Return LPG concentration
-  CH4 =  MQ6.readSensor("CH4"); // Return CH4 concentration
-  CO =  MQ6.readSensor("CO"); // Return CO concentration
-  Alcohol =  MQ6.readSensor("Alcohol"); // Return Alcohol concentration
-
-  Serial.println("***************************");
-  Serial.println("Lectures for MQ-6");
-  Serial.print("Volt: ");Serial.print(MQ6.getVoltage(false));Serial.println(" V"); 
-  Serial.print("R0: ");Serial.print(MQ6.getR0());Serial.println(" Ohm"); 
-  Serial.print("H2: ");Serial.print(H2,2);Serial.println(" ppm");
-  Serial.print("LPG: ");Serial.print(LPG,2);Serial.println(" ppm");
-  Serial.print("CH4: ");Serial.print(CH4,2);Serial.println(" ppm");
-  Serial.print("CO: ");Serial.print(CO,2);Serial.println(" ppm");
-  Serial.print("Alcohol: ");Serial.print(Alcohol,2);Serial.println(" ppm");
-  Serial.println("***************************");  
+  MQ6.update(); // Update data, the arduino will be read the voltage on the analog pin
+  MQ6.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+  MQ6.serialDebug(); // Will print the table on the serial port
+  delay(500); //Sampling frequency
 }
