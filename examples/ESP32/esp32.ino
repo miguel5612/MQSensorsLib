edd573ba0f6cd92f12a633df835e8774fed20604
miguel5612 <miguelangelcu@ufps.edu.co>
2020-04-04 10:12:30 -0500

    Updated example and created release

--- examples/ESP32/esp32.ino
+++ examples/ESP32/esp32.ino
@@ -23,13 +23,13 @@
 //Include the library
 #include <MQUnifiedsensor.h>
 /************************Hardware Related Macros************************************/
-#define         Board                   ("ESP-32")
-#define         Pin                     (A0)  //Analog input 3 of your arduino
+#define         Board                   ("ESP-32") // Wemos ESP-32 or other board, whatever have ESP32 core.
+#define         Pin                     (25)  //IO25 for your ESP32 WeMos Board, pinout here: https://i.pinimg.com/originals/66/9a/61/669a618d9435c702f4b67e12c40a11b8.jpg
 /***********************Software Related Macros************************************/
-#define         Type                    ("MQ-3") //MQ3
+#define         Type                    ("MQ-3") //MQ3 or other MQ Sensor, if change this verify your a and b values.
 #define         Voltage_Resolution      (3.3) // 3V3 <- IMPORTANT. Source: https://randomnerdtutorials.com/esp32-adc-analog-read-arduino-ide/
 #define         ADC_Bit_Resolution      (12) // ESP-32 bit resolution. Source: https://randomnerdtutorials.com/esp32-adc-analog-read-arduino-ide/
-#define         RatioMQ3CleanAir        (60)
+#define         RatioMQ3CleanAir        (60) // Ratio of your sensor, for this example an MQ-3
 /*****************************Globals***********************************************/
 MQUnifiedsensor MQ3(Board, Voltage_Resolution, ADC_Bit_Resolution, Pin, Type);
 /*****************************Globals***********************************************/
